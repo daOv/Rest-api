8eed4b4b7820d86970d9a25f70bac2f7f5e433c8
DamOv <damirovcina@klika.ba>
2018-01-25 14:17:32 +0100

    integration test done

--- src/test/java/com/rest/BookControllerIT.java
+++ src/test/java/com/rest/BookControllerIT.java
@@ -1,5 +1,6 @@
 package com.rest;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.skyscreamer.jsonassert.JSONAssert;
@@ -7,28 +8,29 @@ import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
-@RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@TestPropertySource("test.properties")
+@RunWith(SpringRunner.class)
 public class BookControllerIT {
 
 	@LocalServerPort
 	private int port;
+	private TestRestTemplate template;
 
-	private TestRestTemplate template = new TestRestTemplate();
+	@Before
+	public void setUp() {
+		template = new TestRestTemplate();
+	}
 
 	@Test
 	public void retriveBooks() throws Exception {
-		
-		String expected = "";
 		String uri = "/api/books";
+		String expected = "[{\"id\":1,\"title\":\"Book\",\"description\":\"good Book\"},{\"id\":2,\"title\":\"book\","
+				+ "\"description\":\"bad Book\"},{\"id\":3,\"title\":\"Book\","
+				+ "\"description\":\"very good Book\"},{\"id\":4,\"title\":\"book\",\"description\":\"very bad Book\"}]";
 		ResponseEntity<String> response = template.getForEntity(createUrl(uri), String.class);
-
-		JSONAssert.assertEquals(expected, response.getBody(),false);
+		JSONAssert.assertEquals(expected, response.getBody(), false);
 	}
 
 	private String createUrl(String uri) {
