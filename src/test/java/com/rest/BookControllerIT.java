e09a9535412fa92447c3a7c0893b1bf3106db006
DamOv <damirovcina@klika.ba>
2018-01-25 17:12:26 +0100

    hf

--- src/test/java/com/rest/BookControllerIT.java
+++ src/test/java/com/rest/BookControllerIT.java
@@ -1,6 +1,5 @@
 package com.rest;
 
-import org.json.JSONException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,11 +7,8 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.rest.model.Book;
 
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
@@ -26,9 +22,9 @@ public class BookControllerIT {
 
 	@Before
 	public void setUp() {
-		template = new TestRestTemplate();  
+		template = new TestRestTemplate();
 	}
-	
+
 	@Test
 	public void retriveBooks() throws Exception {
 		String expected = "[{\"id\":1,\"title\":\"Book\",\"description\":\"good Book\"},{\"id\":2,\"title\":\"book\","
@@ -37,16 +33,13 @@ public class BookControllerIT {
 		ResponseEntity<String> response = template.getForEntity(createUrl("api/books"), String.class);
 		JSONAssert.assertEquals(expected, response.getBody(), false);
 	}
-	
+
 	@Test
 	public void retriveBookById() throws Exception {
 		String expected = "{\"id\":1,\"title\":\"Book\",\"description\":\"good Book\"}";
 		ResponseEntity<String> response = template.getForEntity(createUrl("api/books/1"), String.class);
 		JSONAssert.assertEquals(expected, response.getBody(), false);
 	}
-	
-	
-
 
 	private String createUrl(String uri) {
 		return LOCAL_HOST + port + uri;
