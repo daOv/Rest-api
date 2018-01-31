5548d604d12bad09d5a626f1c7d6fcce20be1603
DamOv <damirovcina@klika.ba>
2018-01-31 13:37:10 +0100

    testing done

--- src/test/java/com/rest/BookControllerIT.java
+++ src/test/java/com/rest/BookControllerIT.java
@@ -1,7 +1,7 @@
 package com.rest;
-import org.json.JSONException;
-import org.junit.Assert;
+
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.skyscreamer.jsonassert.JSONAssert;
@@ -14,25 +14,35 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import com.rest.model.Book;
 import com.rest.model.BookCategory;
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.startsWith;
+import org.json.JSONException;
+
+import java.net.URI;
 
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
 public class BookControllerIT {
 
-	private TestRestTemplate template;
+	private static  TestRestTemplate template;
 
 	@LocalServerPort
 	private int port;
 
 	private static final String LOCAL_HOST = "http://localhost:";
 
-	HttpHeaders headers = new HttpHeaders();
+	static HttpHeaders headers;
+	HttpEntity<Book> requestEntity;
+
+	@BeforeClass
+	public static void beforeClassMethod() {
+		template = new TestRestTemplate();
+		headers = new HttpHeaders();
+	}
 
 	@Before
 	public void setUp() {
-		template = new TestRestTemplate();
 
 	}
 
@@ -41,7 +51,6 @@ public class BookControllerIT {
 		String expected = "[{\"id\": 1,\"title\": \"testBook\",\"description\": \"test book \",\"bookCategory\": {\"id\": 1,\"name\": \"testCategory\"}}]";
 		ResponseEntity<String> response = template.getForEntity(createUrl("api/books"), String.class);
 		JSONAssert.assertEquals(expected, response.getBody(), false);
-
 	}
 
 	@Test
@@ -53,15 +62,24 @@ public class BookControllerIT {
 
 	@Test
 	public void createBook() {
-		HttpEntity<Book> request = new HttpEntity<Book>(new Book(2, "myTitle", "myDescription", new BookCategory(1)));
-		HttpEntity<Book> postResponse = template.postForEntity(createUrl("/api/books"), request, Book.class);
-		ResponseEntity<Book> response = template.getForEntity(createUrl("/api/books/2"), Book.class);
-		assertEquals("myDescription", response.getBody().getDescription());
+		requestEntity = new HttpEntity<Book>(new Book(2, "myTitle", "myDescription", new BookCategory(1)));
+		HttpEntity<Book> postResponse = template.postForEntity(createUrl("/api/books"), requestEntity, Book.class);
+		ResponseEntity<Book> getResponse = template.getForEntity(createUrl("/api/books/2"), Book.class);
+		assertEquals("myDescription", getResponse.getBody().getDescription());
 		String actual = postResponse.getHeaders().getLocation().toString();
-		Assert.assertThat(actual, containsString("api/books/2"));
-		Assert.assertThat(actual,startsWith("http://localhost:"));
+		assertThat(actual, containsString("api/books/2"));
+		assertThat(actual,startsWith("http://localhost:"));
+	}
 
-
+	@Test
+	public void updateBook()  {
+		requestEntity = new HttpEntity<Book>(new Book("TestBook", "Book for testing", new BookCategory(1)));
+		URI postResponse = template.postForLocation(createUrl("/api/books"), requestEntity, Book.class);
+		ResponseEntity<Book> getResponse = template.getForEntity(postResponse, Book.class);
+		HttpEntity<Book> putEntity = new HttpEntity<>(new Book("updatedBook","updatedDescription",new BookCategory(2)));
+		template.put(postResponse.toString(),putEntity);
+		ResponseEntity<Book> getResponseAfterUpdate = template.getForEntity(postResponse, Book.class);
+		assertThat(getResponseAfterUpdate.getBody().getDescription(),containsString("updatedDescription"));
 	}
 
 	@Test
@@ -72,10 +90,7 @@ public class BookControllerIT {
 		JSONAssert.assertEquals(expected, response.getBody(), false);
 	}
 
-	@Test
-	public void updateBook() throws JSONException {
-		// template.put(createUrl("/api/book"), ne);
-	}
+
 
 	private String createUrl(String uri) {
 		return LOCAL_HOST + port + uri;
