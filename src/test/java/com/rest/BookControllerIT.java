7a255b27f15c51fe816955a1928b2ed3387fb82c
DamOv <damirovcina@klika.ba>
2018-01-31 11:06:30 +0100

    lkwk

--- src/test/java/com/rest/BookControllerIT.java
+++ src/test/java/com/rest/BookControllerIT.java
@@ -1,11 +1,4 @@
 package com.rest;
-
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.contains;
-import java.net.URI;
-import org.h2.util.New;
 import org.json.JSONException;
 import org.junit.Assert;
 import org.junit.Before;
@@ -21,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import com.rest.model.Book;
 import com.rest.model.BookCategory;
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertEquals;
 
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -59,34 +53,14 @@ public class BookControllerIT {
 
 	@Test
 	public void createBook() {
-		/*
-		 * ResponseEntity<Client> responseEntity =
-		 * restTemplate.postForEntity("/clients", new CreateClientRequest("Foo"),
-		 * Client.class); Client client = responseEntity.getBody();
-		 * assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
-		 * assertEquals("Foo", client.getName());
-		 */
-		/*
-		 * BookCategory category = new BookCategory(1); Book book = new
-		 * Book("Test book", "test", category); HttpEntity<Book> entity = new
-		 * HttpEntity<Book>(book, headers); ResponseEntity<String> response =
-		 * template.exchange(createUrl("/api/books"), HttpMethod.POST, entity,
-		 * String.class); String actual =
-		 * response.getHeaders().get(HttpHeaders.LOCATION).get(0);
-		 * assertTrue(actual.contains("api/books/2"));
-		 */
 		HttpEntity<Book> request = new HttpEntity<Book>(new Book(2, "myTitle", "myDescription", new BookCategory(1)));
 		HttpEntity<Book> postResponse = template.postForEntity(createUrl("/api/books"), request, Book.class);
 		ResponseEntity<Book> response = template.getForEntity(createUrl("/api/books/2"), Book.class);
 		assertEquals("myDescription", response.getBody().getDescription());
-		// ("http://localhost:50639/api/books/2",
-		// postResponse.getHeaders().getLocation());
-		
-		//assertThat(postResponse.getHeaders().getLocation(), contains("api/books/2"));
+		String actual = postResponse.getHeaders().getLocation().toString();
+		Assert.assertThat(actual, containsString("api/books/2"));
+		Assert.assertThat(actual,startsWith("http://localhost:"));
 
-		/*
-		 * myDescription http://localhost:50639/api/books/2
-		 */
 
 	}
 
